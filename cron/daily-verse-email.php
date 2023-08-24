04afb010a560d4023fd8b344e303fb106cd82e4e
Abe Ramseyer <abe.ramseyer@gmail.com>
2023-08-23 21:33:15 -0500

    fix: email cron no longer has debug info
    fix: only email users that have been active within one month of the currently active schedule

--- cron/daily-verse-email.php
+++ cron/daily-verse-email.php
@@ -8,14 +8,27 @@ require __DIR__."/../www/inc/functions.php";
 $db = new SQLite3(DB_PATH);
 
 $today = new Datetime();
-$today = new Datetime('2023-08-25');
+
 $schedule = get_active_schedule();
+$recently = new Datetime($schedule['start_date']);
+$recently->modify('-1 month');
+
 $scheduled_reading = get_reading($today);
 
 if ($scheduled_reading) {
-  foreach(select("SELECT name, email, trans_pref FROM users WHERE email_verses = 1") as $user) {
-    $name = ucwords(explode(' ', $user['name'])[0]);
-    $html = html_for_scheduled_reading($scheduled_reading, $user['trans_pref'], true);
-    send_daily_verse_email($user['email'], $name, $html);
+  foreach(select("SELECT name, email, trans_pref, last_seen FROM users WHERE email_verses = 1") as $user) {
+    // if a user hasn't been active near the period of the schedule, we won't email them
+    $last_seen_date = new Datetime('@'.$user['last_seen']);
+    if ($recently <= $last_seen_date) {
+      // make the name using everything but the last name
+      $name_arr = explode(' ', $user['name']);
+      $name = array_pop($name_arr);
+      if ($name_arr) {
+        $name = implode(' ', $name_arr);
+      }
+
+      $html = html_for_scheduled_reading($scheduled_reading, $user['trans_pref'], true);
+      send_daily_verse_email($user['email'], $name, $html);
+    }
   }
 }
