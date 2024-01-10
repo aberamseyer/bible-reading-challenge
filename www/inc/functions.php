c7665dbae0abe52bd5dd9acdb1262fc6d9a8e350
Abe Ramseyer <abe.ramseyer@gmail.com>
2024-01-10 09:45:05 -0600

    specific directories

--- www/inc/functions.php
+++ www/inc/functions.php
@@ -1,8 +1,8 @@
 <?php
 
-	require_once "MailSender/Mailsender.php";
-	require_once "MailSender/MailsenderSES.php";
-	require_once "MailSender/MailsenderSendgrid.php";
+	require_once __DIR__."./MailSender/Mailsender.php";
+	require_once __DIR__."./MailSender/MailsenderSES.php";
+	require_once __DIR__."./MailSender/MailsenderSendgrid.php";
 
 	function db($alt_db = null) {
 		static $db;
