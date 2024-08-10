ce5fdb4f8345bc2e550f240c894d071399499832
Abe Ramseyer <abe.ramseyer@gmail.com>
2024-08-10 14:31:52 -0400

    defaults for stats

--- www/inc/BibleReadingChallenge/Site.php
+++ www/inc/BibleReadingChallenge/Site.php
@@ -568,11 +568,11 @@ class Site extends SiteRegistry {
       $timer->mark('user');
       $badges = badges_for_user($user_id);
       $timer->mark('badges');
-      $last_seen = $redis->get_last_seen($user_id) ?: $user['last_seen'];
+      $last_seen = (int) $redis->get_last_seen($user_id) ?: $user['last_seen'];
       $timer->mark('last_seen');
-      $last_read_ts = $this->db->col("SELECT MAX(timestamp) FROM read_dates WHERE user_id = $user[id]");
+      $last_read_ts = (int) $this->db->col("SELECT MAX(timestamp) FROM read_dates WHERE user_id = $user[id]");
       $timer->mark('last_read_ts');
-      $chapters_ive_read = $this->db->col(
+      $chapters_ive_read = (int) $this->db->col(
         sprintf($chp_qry =
           "SELECT SUM(chapters_read)
           FROM (
@@ -585,19 +585,19 @@ class Site extends SiteRegistry {
             HAVING COUNT(*) >= c.verses
         )", "rd.user_id = $user_id"));
       $timer->mark('chapters_ive_read');
-      $words_ive_read = $this->words_read($user);
+      $words_ive_read = (int) $this->words_read($user);
       $timer->mark('words_ive_read');
-      $deviation = $this->deviation_for_user($user_id);
+      $deviation = (float) $this->deviation_for_user($user_id);
       $timer->mark('deviation');
-      $on_target = $this->on_target_percent_for_user($user_id);
+      $on_target = (float) $this->on_target_percent_for_user($user_id);
       $timer->mark('on_target');
-      $all_club_chapters_read = $this->db->col(sprintf($chp_qry, "1"));
+      $all_club_chapters_read = (int) $this->db->col(sprintf($chp_qry, "1"));
       $timer->mark('all_club_chapters_read');
-      $all_club_words_read = $this->words_read();
+      $all_club_words_read = (int) $this->words_read();
       $timer->mark('words_read');
       $progress_graph_data = $this->progress($user_id);
       $timer->mark('progress_graph_data');
-      $on_target_perc = $this->on_target_percent_for_user($user_id);
+      $on_target_perc = (float) $this->on_target_percent_for_user($user_id);
       $timer->mark('on_target_percent');
       $days_behind = 
         $this->db->col("SELECT COUNT(*) FROM schedule_dates WHERE schedule_id = ".$active_schedule->ID." AND date <= '".date('Y-m-d')."'") - 
