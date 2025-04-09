4a696b87853ed149f7fab33c80c4ca6039b1b4bb
Abe Ramseyer <abe.ramseyer@gmail.com>
2025-04-09 16:47:08 -0500

    add goatcounter stats

--- www/inc/foot.php
+++ www/inc/foot.php
@@ -3,7 +3,7 @@
     <?= navigation() ?>
   <?php endif; ?>
   <?= $add_to_foot ?>
-
+  <script data-goatcounter="https://abe-brc.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
   <!-- Request took <?= number_format(1_000*(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']), 1) ?>ms -->
   </body>
 </html>
