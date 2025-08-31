86e3b2d2f052782f7b52fef0ac0222bf5843eaf8
Abe Ramseyer <abe@ramseyer.dev>
2025-08-31 16:02:49 -0500

    add indexes to migrations file

--- migrations/9_email_stats.sql
+++ migrations/9_email_stats.sql
@@ -7,3 +7,8 @@ CREATE TABLE verse_email_stats (
 	clicked_done_timestamp TEXT,
 	CONSTRAINT emails_users_FK FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+CREATE INDEX idx_verse_email_stats_email_id ON verse_email_stats(email_id);
+CREATE INDEX idx_verse_email_stats_user_id ON verse_email_stats(user_id);
+CREATE INDEX idx_verse_email_stats_schedule_date_id ON verse_email_stats(
+  schedule_date_id
+);
